cc3f18e4e36f7cb0ccbc05d0d8c2efa0357b420d
John Lenz <john.lenz27@gmail.com>
2023-01-11 14:19:25 -0800

    it will collect the info from the form

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,13 @@ function hideResults(){
     document.getElementById("emily").setAttribute("class", "hidden");
     document.getElementById("sorry").setAttribute("class", "hidden");
 }
+
+window.onload = function() {
+    document.querySelector("form").onsubmit = function(event) {
+      event.preventDefault();
+      hideResults();
+      const age = parseInt(document.querySelector("input#age").value);
+      const height = parseInt(document.querySelector("input#height").value);
+    };
+};
+
