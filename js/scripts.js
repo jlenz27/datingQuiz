087309bfc5b014f7262c6892dab4371007cfdb3b
John Lenz <john.lenz27@gmail.com>
2023-01-11 14:17:18 -0800

    it will hide the branchs with java

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// user Interface Logic
+
+function hideResults(){
+    document.getElementById("sarah").setAttribute("class", "hidden");
+    document.getElementById("ashley").setAttribute("class", "hidden");
+    document.getElementById("emily").setAttribute("class", "hidden");
+    document.getElementById("sorry").setAttribute("class", "hidden");
+}
